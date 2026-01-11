d0b0e46dd72857e72ed5a881146c587f6bc295de
Ephy <macbookpro@Ephys-MacBookPro.local>
2026-01-12 02:05:11 +0300

    done

--- lib/testing/codegrade_test.py
+++ lib/testing/codegrade_test.py
@@ -1,4 +1,12 @@
 
+from lib.kickstarter_scraper import create_project_dict
+
 def test_codegrade_placeholder():
     """Codegrade placeholder test"""
     assert 1==1
+
+def test_scraper_returns_dict():
+    """Test that scraper returns a dictionary"""
+    projects = create_project_dict()
+    assert isinstance(projects, dict)
+    assert len(projects) > 0
